3eed262c613f63ee28a7d201e8df89042cd78bb8
@Ash9311 <ashutoshmulki7@gmail.com>
2023-03-11 16:00:42 +0530

    populate update product form

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from './../services/product.service';
+import { product } from './../data-type';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -6,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent implements OnInit {
-
-  constructor() { }
+  productData: undefined | product;
+  constructor(private route: ActivatedRoute, private product: ProductService) { }
 
   ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    productId && this.product.getProduct(productId).subscribe((data) => {
+      console.log(data);
+      this.productData = data;
+    });
   }
 
-  submit(data:any){
+  submit(data: any) {
 
   }
 
